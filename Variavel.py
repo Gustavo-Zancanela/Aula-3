8a22733978e40c9a05a95e8115b4658090349672
teste <dalybie@gmail.com>
2024-10-30 14:39:41 -0300

    V0.1

---
+++ Variavel.py
@@ -0,0 +1,24 @@
+import math
+var = 1
+resultado = 1000.0
+nome_cliente = "José"
+
+print(var, resultado, nome_cliente)
+
+var1 = "3"
+print("Versão Python: " + var1)
+
+var2 = 1
+print(var2)
+var2 += 1
+print(var2)
+
+var3 = 100
+var3 = 200 + 300
+print(var3)
+
+a = 3.0
+b = 4.0
+c = (a**2 + b**2) ** 0.5
+d = math.sqrt(a**2 + b**2)
+print(d)
---
+++ dados literais.py
@@ -0,0 +1,92 @@
+print(0.00000005)
+print(50000000)
+print(-0.000000018)
+
+# Adição - Operadores unários e binários
+print('Adição')
+print(2+2)
+print(+2) #unários
+print(-4 + 4) #binário
+print(-4. + 8) #binário
+print()
+
+# Subtração - Operadores unários e binários
+print('Subtração')
+print(2-2)
+print(-1.8) #unários
+print(-4 - 4) #binário
+print(4. - 8) #binário
+print()
+
+# Multiplicação
+print('Multiplicação')
+print(2 * 3)
+print(2. * 3)
+print(2 * 3.)
+print(2. * 3.)
+print()
+
+# Divisão
+print('Divisão')
+print(6 / 3)
+print(6. / 3)
+print(6 / 3.)
+print(6. / 3.)
+print()
+
+# Divisão arredondada
+print('Divisão arredondada')
+print(10 // 3)
+print(10. // 3)
+print(10 // 3.)
+print(10. // 3.)
+print()
+
+# Arredonda para baixo, nunca para cima
+print(6 // 4)
+print(6. // 4)
+print(3.8 / 2)
+print(2.4 / 2)
+print(2.4 // 2)
+print()
+ 
+# Divisão de piso
+print(-6 / 4)
+print(-6 // 4)
+print(11 / 3)
+print(11 // 3)
+print()
+
+# Resto da divisão
+# Jamais tente dividir por zero
+print('Resto da divisão')
+print(14 % 4)
+print(12 % 4.5)
+print()
+
+# Exponencial
+print('Exponencial')
+print(2 ** 3)
+print(2. ** 3)
+print(2 ** 3.)
+print(2. ** 3.)
+print()
+
+# Hierarquia de Prioridades
+print("Hierarquia de Prioridades")
+print(2+3*5)
+print(9 % 6 % 2)
+print(2**2**3)
+print()
+
+# Parênteses
+print('Parênteses')
+print(-3 ** 2)
+print((-3) ** 2)
+print(-2 ** 3)
+print((-2) ** 3)
+print(-(3**2))
+print()
+print((5 *((25 % 13) + 100 ) / (2 * 13)) // 2)
+
+
